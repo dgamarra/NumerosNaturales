0760ffa0caa9d0436fa25f0d69f08f2008f4698f
dgamarra <dgamarra@continental.edu.pe>
2024-04-12 20:27:04 -0500

    Se añadio el Calcula el mcm

--- main.py
+++ main.py
@@ -14,9 +14,23 @@ class CalculadoraMCD:
             a, b = b, a % b
         return a
 
+    def calcular_mcm(self, a, b):
+        """
+        Calcula el mínimo común múltiplo (mcm) de dos números enteros.
+
+        Argumentos:
+        a -- Primer número entero
+        b -- Segundo número entero
+
+        Retorna:
+        El mínimo común múltiplo de a y b
+        """
+        return abs(a * b) // self.calcular_mcd(a, b)
+
 
 # Ejemplo de uso:
 calculadora = CalculadoraMCD()
 num1 = 48
 num2 = 18
 print(f"El MCD de {num1} y {num2} es: {calculadora.calcular_mcd(num1, num2)}")
+print(f"El mcm de {num1} y {num2} es: {calculadora.calcular_mcm(num1, num2)}")
